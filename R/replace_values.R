c38ba6d00127ecf532da789139df9cb7f8f735e9
motoki <motoki@saito.sfs.uni-tuebingen.de>
2020-06-04 12:57:37 +0900

    Fixed replace_values(), so it now handles with a vector, not a dataframe.

--- R/replace_values.R
+++ R/replace_values.R
@@ -1,29 +1,28 @@
-#' Value replacement in a dataframe
+#' Multiple value replacement in a vector
 #'
-#' Replaces multiple values in a dataframe according to pairs provided.
+#' Replaces multiple values in a vector according to pairs provided.
 #'
-#' @param dat A dataframe to which you want to execute replacements.
+#' @param vec A vector to which you want to execute replacements.
 #' @param from A vector of strings which you want to replace for something else.
 #' @param to A vector of strings with the same length as 'from'. Target strings specified by 'from' are replaced by the corresponding values specified by this variable ('to').
-#' @param clm A string, which gives the name of the column whose values you want to replace.
-#' @return A new dataframe, whose column with the name specified by 'clm' now contains new replaced values.
+#' @return A new vector with new replacement values.
 #' @author Motoki Saito, \email{motoki.saito@uni-tuebingen.de}
 #' @keywords utilities
 #' @examples
-#' ddd = data.frame('letter'=LETTERS[1:5], 'value'=seq(10,50,10))
+#' vvv = LETTERS[1:5]
 #' fff = c('B', 'D')
 #' ttt = c('XX', 'YY')
-#' dat = replace_values(dat=ddd, from=fff, to=ttt, clm='letter')
-#' print(dat)
+#' vvv = replace_values(vec=vvv, from=fff, to=ttt)
+#' print(vvv)
 #' @export
 #'
-replace_values = function ( dat, from=c('aː', 'a', 'iː', 'ɪ', 'uː', 'ʊ'), to=c('a',  'a', 'i',  'i', 'u',  'u'), clm='Segment') {
-	if (is.factor(dat[[clm]])) {
-		dat[[clm]] = as.character(dat[[clm]])
+replace_values = function ( vec, from=c('aː', 'a', 'iː', 'ɪ', 'uː', 'ʊ'), to=c('a',  'a', 'i',  'i', 'u',  'u') ) {
+	if (is.factor(vec)) {
+		vec = as.character(vec)
 	}
 	for ( i in seq_along(from) ) {
-		dat[[clm]][dat[[clm]]==from[i]] = to[i]
+		vec[vec==from[i]] = to[i]
 	}
-	return(dat)
+	return(vec)
 }
 
