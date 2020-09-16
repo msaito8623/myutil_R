67181119d28e6714ee5e1c60a8da736a4070071c
motoki <motoki@saito.sfs.uni-tuebingen.de>
2020-09-16 17:40:35 +0200

    Minor fix: Examples in normalize()

--- R/normalize.R
+++ R/normalize.R
@@ -8,20 +8,20 @@
 #' @author Motoki Saito, \email{motoki.saito@uni-tuebingen.de}
 #' @keywords utilities
 #' @examples
-set.seed(0)
-vec00 = rnorm(100)
-vec01 = normalize(vec00)
-vec02 = normalize(vec00, norm.range=c(-0.5,0.5))
-par(mfrow=c(1,3))
-rng00 = round(range(vec00), 2)
-rng01 = round(range(vec01), 2)
-rng02 = round(range(vec02), 2)
-ttl00 = paste(as.character(rng00),collapse=', ')
-ttl01 = paste(as.character(rng01),collapse=', ')
-ttl02 = paste(as.character(rng02),collapse=', ')
-hist(vec00, main=sprintf('vec00, range=[%s]', ttl00))
-hist(vec01, main=sprintf('vec01, range=[%s]', ttl01))
-hist(vec02, main=sprintf('vec02, range=[%s]', ttl02))
+#' set.seed(0)
+#' vec00 = rnorm(100)
+#' vec01 = normalize(vec00)
+#' vec02 = normalize(vec00, norm.range=c(-0.5,0.5))
+#' par(mfrow=c(1,3))
+#' rng00 = round(range(vec00), 2)
+#' rng01 = round(range(vec01), 2)
+#' rng02 = round(range(vec02), 2)
+#' ttl00 = paste(as.character(rng00),collapse=', ')
+#' ttl01 = paste(as.character(rng01),collapse=', ')
+#' ttl02 = paste(as.character(rng02),collapse=', ')
+#' hist(vec00, main=sprintf('vec00, range=[%s]', ttl00))
+#' hist(vec01, main=sprintf('vec01, range=[%s]', ttl01))
+#' hist(vec02, main=sprintf('vec02, range=[%s]', ttl02))
 #' @export
 #'
 normalize = function ( x, norm.range=c(0,1)) {
